004e76ab6f4afc0c96c4324071f91db406b0ad7e
Vaidehi Sridhar <vaidehi407@gmail.com>
2022-12-23 13:55:33 +0530

    Bruh

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 let express=require('express');
 let bodyParser=require('body-parser');
 let ejs = require("ejs");
+const mongoose=require('mongoose');
+let newUsers=[]
+mongoose.set("strictQuery", false);
 
+
+mongoose.connect('mongodb://127.0.0.1:27017/projectValuedDB');
+
+/*
 let newUsers=[];
 let loggedInUsers=[];
 let EditedProfiles=[];
-
+*/
 let app=express();
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
+
+
+
+const credsSchema = {
+  username: String,
+  email:String,
+  password:String,
+  password1:String
+};
+
+const Credential = mongoose.model("Credential", credsSchema);
+
+
+
 
 app.get("/",function(req,res){
     res.render("index");
@@ -18,48 +39,87 @@ app.get("/",function(req,res){
 
   app.get("/login",function(req,res){
     res.render("login");
-  });
-
   
-  app.get("/dashboard",function(req,res){
-    res.render("dashboard",{newUsers1:newUsers,EditedProfiles1:EditedProfiles});
   });
+  app.get("/signup",function(req,res){
+    res.render("signup");
+  });
+ 
+  
+  
 
   app.get("/editprofile",function(req,res){
     res.render("editprofile",{newUsers1:newUsers, EditedProfiles1:EditedProfiles});
   });
 
 
+  app.post("/signup",function(req,res){
 
-  app.post("/login",function(req,res){
-    const newUser={
-        username:req.body.username,
-    email:req.body.email,
-    password:req.body.createps,
-    password1:req.body.confirmps
-     } ;
-            var at=newUser.email.indexOf('@');
-            var dot=newUser.email.lastIndexOf('.');
-            if(!((at==-1)||(dot==-1)||(dot-at<2)||(newUser.password!=newUser.password1))){
-                newUsers.push(newUser);
-                const loggedInUser={
 
-                    email:req.body.email,
-                password:req.body.createps,
-            
-                 }
-            
-                 if((newUser.email===loggedInUser.email) && (newUser.password===loggedInUser.password) )
-                 {
-                    //console.log(loggedInUser);
-                    res.redirect("/dashboard");
-                 }
 
-            }
-           
+    const newUsername=req.body.username;
+    const newEmail=req.body.email;
+    const newPassword=req.body.createps;
+     const newPassword1=req.body.confirmps;
+      const profile = new Credential({
+       username: newUsername,
+   email:newEmail,
+   password:newPassword,
+   password1:newPassword1
+     });
+ 
+   var newUsers=Credential.find({},function(err){});
+    
+         profile.save();
+         res.redirect("/dashboard")
+ 
+      
+   });
+
+
+   app.get("/dashboard",function(req,res){
+    res.render("dashboard",{newUsers1:newUsers, EditedProfiles1: newUsers});
+
+  });
+
+  // app.post("/login",function(req,res){
+
+
+
+  //  const newUsername=req.body.username;
+  //  const newEmail=req.body.email;
+  //  const newPassword=req.body.createps;
+  //   const newPassword1=req.body.confirmps;
+  //    const profile = new Credential({
+  //     username: newUsername,
+  // email:newEmail,
+  // password:newPassword,
+  // password1:newPassword1
+  //   });
+
+    
+  //   const loginEmail=req.body.email1;
+  //   const loginpwd=req.body.password1;
+  //   Credential.find({email:loginEmail}, function(err,foundCredentials){
+  //     if(err)
+  //     console.log("WG");
+  //     else if (!(foundCredentials)){
+  //       profile.save();
+
+
+  //       res.redirect("/dashboard")
+
+
+  //     }
      
-  })
 
+  //   })
+     
+  // });
+
+  
+  
+ 
 
 
 
@@ -94,6 +154,3 @@ app.get("/",function(req,res){
      console.log("Project management made easier @  http://localhost:3000/");
      });
 
-
-
-  
